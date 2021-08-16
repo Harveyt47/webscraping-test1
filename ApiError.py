addb388c6c44ec32b5699c195f36af31e7bbf154
harveyt47 <harveythompson0@gmail.com>
2021-08-16 13:58:32 +1000

    adding

---
+++ ApiError.py
@@ -0,0 +1,25 @@
+from scrapinghub import ScrapinghubClient
+import sys
+def ApiError(apikey, project_id):
+    client = ScrapinghubClient(apikey)
+    project = client.get_project(project_id)
+    spiders = project.spiders
+    spider_list = spiders.list()
+    matrix = {"include": []}
+    for spider in spider_list:
+        name = spider['id']
+        store = spiders.get(name)
+        jobs = store.jobs
+        jobs_list = jobs.list()
+        job_key = jobs_list[0]['key']
+        job = project.jobs.get(job_key)
+        error_logs = job.logs.list(level='ERROR')
+        if error_logs:
+            message = ''
+            for log in error_logs:
+                message= message+' ' +log['message'] 
+            matrix["include"].append({'spider':name, 'message':message})
+    return matrix
+
+if __name__ == "__main__":
+    ApiError(sys.argv[1], sys.argv[2])
