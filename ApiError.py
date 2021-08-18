bb8fe3c4038498d251a0335d95222933efe6959c
Harvey Thompson <58153750+Harveyt47@users.noreply.github.com>
2021-08-18 14:46:03 +1000

    Update ApiError.py

--- ApiError.py
+++ ApiError.py
@@ -5,8 +5,7 @@ def ApiError(apikey, project_id):
     project = client.get_project(project_id)
     spiders = project.spiders
     spider_list = spiders.list()
-    error_matrix = {"include": []}
-    item_matrix = {"include": []}
+    matrix = {"include": []}
     for spider in spider_list:
         name = spider['id']
         store = spiders.get(name)
@@ -16,16 +15,16 @@ def ApiError(apikey, project_id):
         new_job = project.jobs.get(new_job_key)
         error_logs = new_job.logs.list(level='ERROR')
         if error_logs:
-            message = ''
+            message = b''
             for log in error_logs:
-                message = message + ' ' + log['message'] 
-            error_matrix["include"].append({'spider':name, 'message':message})
+                message = message + b' ' + log[b'message'] 
+            matrix["include"].append({'spider':name, 'message':message, 'type':'ERROR'})
         else:
             try:
                 num_items_new_job = jobs_list[0]['items']
                 
             except:
-                item_matrix["include"].append({'spider':name, 'message':'has no items'})
+                matrix["include"].append({'spider':name, 'message':'has no items', 'type':'ITEM'})
                 continue
                 
             # the loop below searchs for the first old job that has items
@@ -40,14 +39,14 @@ def ApiError(apikey, project_id):
             
             if num_items_old_job:
                 if num_items_old_job != num_items_new_job:
-                    item_matrix["include"].append({'spider':name, 'message':'The number of items returned has changed. Please investigate'})
+                    matrix["include"].append({'spider':name, 'message':'The number of items returned has changed. Please investigate', 'type':'ITEM'})
                 else:
                     continue
             else:
                 continue
                 
                 
-    return error_matrix, item_matrix
+    return matrix
 
 if __name__ == "__main__":
     ApiError(sys.argv[1], sys.argv[2])
