8dbaa76274786924358af2e5b1975df089ced619
harveyt47 <harveythompson0@gmail.com>
2021-08-18 16:05:30 +1000

    revert

--- ApiError.py
+++ ApiError.py
@@ -11,40 +11,13 @@ def ApiError(apikey, project_id):
         store = spiders.get(name)
         jobs = store.jobs
         jobs_list = jobs.list()
-        new_job_key = jobs_list[0]['key']
-        new_job = project.jobs.get(new_job_key)
-        error_logs = new_job.logs.list(level='ERROR')
+        job_key = jobs_list[0]['key']
+        job = project.jobs.get(job_key)
+        error_logs = job.logs.list(level='ERROR')
         if error_logs:
-            message = 'has an error please fix'
-
-            matrix["include"].append({'spider':name, 'message':message, 'type':'ERROR'})
-        else:
-            try:
-                num_items_new_job = jobs_list[0]['items']
-                
-            except:
-                matrix["include"].append({'spider':name, 'message':'has no items', 'type':'ITEM'})
-                continue
-                
-            # the loop below searchs for the first old job that has items
-            num_items_old_job=0
-            for job in jobs_list[1:]:
-                try:
-                    num_items_old_job = job['items']
-                    old_job_key = job['key']
-                    break
-                except:
-                    continue
-            
-            if num_items_old_job:
-                if num_items_old_job != num_items_new_job:
-                    matrix["include"].append({'spider':name, 'message':'The number of items returned has changed. Please investigate', 'type':'ITEM'})
-                else:
-                    continue
-            else:
-                continue
-                
-                
+            message = 'has error'
+            matrix["include"].append({'spider':name, 'message':message})
+    print(matrix)
     return matrix
 
 if __name__ == "__main__":
